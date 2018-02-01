917bf90e40a55005a469877c8e04de77cc16f435
Martin Mucito <martin.mucito@gmail.com>
2018-02-01 17:41:04 -0600

    fix limit

--- index.js
+++ index.js
@@ -117,8 +117,8 @@ var proxy = httpProxy.createProxyServer({
 
 var app = express();
 
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 
 
 app.use(compress());
