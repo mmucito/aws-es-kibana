40a82343ea214fe86caccbb9a11f1107f8acf67c
Martin Mucito <martin.mucito@gmail.com>
2018-02-02 15:52:29 -0600

    test

--- index.js
+++ index.js
@@ -122,8 +122,9 @@ var proxy = httpProxy.createProxyServer({
 var app = express();
 //app.disable('etag');
 
-//app.use(bodyParser.json({limit: "1mb"}));
-app.use(bodyParser.urlencoded({limit: "1mb", extended: true, parameterLimit:50000}));
+app.use(bodyParser());
+app.use(bodyParser({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb'}));
 
 
 app.use(compress());
