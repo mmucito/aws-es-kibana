946390aaf60a4677de33f490016fd4d309517f87
Martin Mucito <martin.mucito@gmail.com>
2018-02-02 15:48:32 -0600

    test

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ var app = express();
 //app.disable('etag');
 
 app.use(bodyParser.json({limit: "1mb"}));
-app.use(bodyParser.urlencoded({limit: "1mb", extended: true, parameterLimit:50000}));
+//app.use(bodyParser.urlencoded({limit: "1mb", extended: true, parameterLimit:50000}));
 
 
 app.use(compress());
