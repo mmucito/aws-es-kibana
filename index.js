e8ece2ec16e8ad7837b70ef81cb3591e461c5a79
Martin Mucito <martin.mucito@gmail.com>
2018-02-02 15:38:33 -0600

    fix

--- index.js
+++ index.js
@@ -120,10 +120,10 @@ var proxy = httpProxy.createProxyServer({
 });
 
 var app = express();
-app.disable('etag');
+//app.disable('etag');
 
-app.use(bodyParser.json({limit: "50mb"}));
-app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+app.use(bodyParser.json({limit: "5mb"}));
+app.use(bodyParser.urlencoded({limit: "5mb", extended: true, parameterLimit:50000}));
 
 
 app.use(compress());
